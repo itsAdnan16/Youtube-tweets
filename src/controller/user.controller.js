23e4cf18f159ff357ba3f028879f44678f2fd855
Adnan <E22CSEU1269@Bennett.edu.in>
2025-07-25 03:54:07 +0530

    completed the register controller

---
+++ src/controller/user.controller.js
@@ -0,0 +1,58 @@
+
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
+const registerUser = asyncHandler(async (req,res)=>{
+    
+    //first i have to get information from the frontend
+    const {fullName,email,username,password} = req.body;
+
+    //check if anyone of this detail is missing
+    if(!fullName || !email || !username || !password){
+        throw new ApiError(400 , "Please provide all the details");
+    }
+
+    //check if the user already exists
+    const existingUser = await User.findOne({
+        $or : [{email},{username}]
+    });
+
+    if(existingUser){
+        throw new ApiError(409,"User with email or username already exists")
+    }
+
+    //checking if the avatar is uploaded or not
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+
+     let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Please upload an avatar image");
+    }
+
+    //upload the avatar file on cloudinary
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    //if there is some error while uploading the avatar
+    if(!avatar){
+        throw new ApiError(500,"Error while uploading the avatar image");
+    }
+
+    const user = await User.coverImage
+
+
+
+
+
+
+})
+
+export {registerUser};
